76a6c7c3e33305b404e715faf0ff3e0b2663ac2e
Raghu Ballu <raghu.ballu@gmail.com>
2021-12-30 19:49:44 +0530

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import requests
-def sendMessage():
-
-    parameters = {"chat_id": "-606084987", "text": "Hello hi"}
-    response = requests.get(base_url + "/sendMessage", data = parameters)
-sendMessage()
